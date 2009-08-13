93dfa1f9591cf0077849c8c88c29e2f2db3d974d
josecuadra <josecuadra@314be7ac-7ab7-11de-98bd-fdc22f85cdda>
2009-08-13 15:09:45 +0000

    modified first-build-all script to exclude the portal-webservice project not in use anymore

--- portal-web/first-build-all.sh
+++ portal-web/first-build-all.sh
@@ -7,10 +7,5 @@ mvn install:install-file -Dfile=target/portal-index-1.0-SNAPSHOT-sources.jar -Da
 cd ../portal-service
 mvn -U -Dmaven.test.skip=true clean install source:jar
 mvn install:install-file -Dfile=target/portal-service-1.0-SNAPSHOT-sources.jar -DartifactId=portal-service -DgroupId=portal -Dpackaging=zip -Dversion=1.0-SNAPSHOT
-cd ../portal-webservices/binding
-mvn -U install
-cd ..
-mvn -U -Dmaven.test.skip=true install source:jar
-mvn install:install-file -Dfile=target/portal-webservices-1.0-SNAPSHOT-sources.jar -DartifactId=portal-webservices -DgroupId=portal -Dpackaging=zip -Dversion=1.0-SNAPSHOT
 cd ../portal-web
 mvn -U install
